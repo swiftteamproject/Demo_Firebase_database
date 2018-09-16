024b4ecb31ab16c3ae0ca5602605902603022271
Зайка <samentyev@gmail.com>
2018-09-16 13:58:35 +0300

    Initial Commit

---
+++ Demo_Firebase_database/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo_Firebase_database
+//
+//  Created by Артем Валерьевич on 16/09/2018.
+//  Copyright © 2018 Артем Валерьевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
